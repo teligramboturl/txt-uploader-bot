a7e386c0b022b234dfc43e44a5507b2ad153a186
Rajesh R Mahar <136462044+rajrmahar@users.noreply.github.com>
2025-02-21 14:05:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ async def start(client: Client, msg: Message):
     await asyncio.sleep(1)
     await start_message.edit_text(
         Data.START.format(msg.from_user.mention) +
-        "Checking status Ok... \n\n I Am A Bot For Download Files From Your **.TXT** File And Then Upload That File On Telegram So Basically If You Want To Use Me First Send Me \n/rajesh,\n/mahar,\n/upload,\n/txt,\n/txt1\nCommand And Then Follow Few Steps..\n\nUse /stop to stop any ongoing task **Bot Made BY Rajesh Mahar 🕊️🕊️**🔍\n\n"
+        "Checking status Ok... \n\n I Am A Bot For Download Files From Your **.TXT** File And Then Upload That File On Telegram So Basically If You Want To Use Me First Send Me \n/rajesh\n/mahar\n/upload\n/txt\n/txt1\nCommand And Then Follow Few Steps..\n\nUse /stop to stop any ongoing task **Bot Made BY Rajesh Mahar 🕊️🕊️**🔍\n\n"
         "Progress:[🟩🟩🟩🟩🟩🟩🟩🟩🟩] 100%\n\n", reply_markup=InlineKeyboardMarkup(
             [
                 [
